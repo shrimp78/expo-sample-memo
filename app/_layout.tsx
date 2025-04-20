21d77b5d05ec93fbc5b1a9a9b236510a638e5daa
Yoshiaki Kitamura <yoshiakikitamura@MacBook-Air.local>
2025-04-20 13:45:03 +0900

    Add libraries

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import { config } from '@gluestack-ui/config'
+import { GluestackUIProvider} from '@gluestack-ui/themed'
+import { Stack } from 'expo-router'
+
+export default function Layout() {
+  return (
+    <GluestackUIProvider config={config}>
+      <Stack screenOptions={{ headerTintColor: '#000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
+
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+
+        {/* ホーム */}
+        <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
+      </Stack>
+    </GluestackUIProvider>
+  )
+}
+
