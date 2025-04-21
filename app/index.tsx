f56fad6406c57864a974005e11ec524f02fe69b1
shrimp78 <y.kitamura78@gmail.com>
2025-04-21 11:51:09 +0900

    スプラッシュ画面にcssを追加

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,23 @@
 //import {router } from 'expo-router'
-import { Text, View} from 'react-native'
+import { StyleSheet, Text, View} from 'react-native'
 
 export default function InitialScreen() {
   return (
-  <View>
-    <Text>アプリ起動中</Text>
+  <View style={styles.container}>
+    <Text style={styles.title}>アプリ起動中</Text>
   </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FEFEF4',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+})
