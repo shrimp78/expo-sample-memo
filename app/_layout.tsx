9c58aa0f19a72c2c6c1fd7102097871cf026a573
shrimp78 <y.kitamura78@gmail.com>
2025-04-23 10:43:13 +0900

    prettierの設定

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,15 @@
-import { config } from '@gluestack-ui/config'
-import { GluestackUIProvider} from '@gluestack-ui/themed'
-import { Stack } from 'expo-router'
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
     <GluestackUIProvider config={config}>
       <Stack screenOptions={{ headerTintColor: '#000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
-
         <Stack.Screen name="index" options={{ headerShown: false }} />
         {/* ホーム */}
         <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
-
       </Stack>
     </GluestackUIProvider>
-  )
+  );
 }
-
