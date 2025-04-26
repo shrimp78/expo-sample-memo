c92789f7b49517dc40dce59ed59f0ff7cb68c875
shrimp78 <y.kitamura78@gmail.com>
2025-04-26 12:49:19 +0900

    整

--- app/items/index.tsx
+++ app/items/index.tsx
@@ -1,14 +1,19 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { useState } from 'react';
 import { Item } from '../../src/components/types/item';
 import { ItemList } from '../../src/components/items/ItemList';
 import { DUMMY_ITEMS } from '../../src/data/dummyItemData';
+
 export default function ItemScreen() {
   const [items, setItems] = useState<Item[]>([]);
 
   return (
     <View style={styles.container}>
-      <FlatList data={DUMMY_ITEMS} renderItem={({ item }) => <ItemList name={item.title} content={item.content} />} />
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 120 }}
+        data={DUMMY_ITEMS}
+        renderItem={({ item }) => <ItemList name={item.title} content={item.content} />}
+      />
     </View>
   );
 }
