a98d4563e4224386bddb78b60af189ce6de86014
shrimp78 <y.kitamura78@gmail.com>
2025-04-29 11:42:13 +0900

    詳細画面もInputFormを適用

--- app/items/[id].tsx
+++ app/items/[id].tsx
@@ -1,28 +1,37 @@
-import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Item } from '../../src/components/types/item';
+import { KeyboardAvoidingView } from '@gluestack-ui/themed';
 import { ItemInputForm } from '../../src/components/items/ItemInputForm';
 import { DUMMY_ITEMS } from '../../src/data/dummyItemData';
 
 export default function ItemEditScreen() {
   const { id } = useLocalSearchParams();
 
-  const [item, setItem] = useState<Item | null>(null);
+  // タイトルと内容の状態
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
   useEffect(() => {
     console.log('useEffect -> id', id);
     const item = DUMMY_ITEMS.find(item => item.id === Number(id));
     if (item) {
-      setItem(item);
+      setTitle(item.title);
+      setContent(item.content);
     }
     // TODO: アイテムが見つからない場合の処理を後で追加する
   }, [id]);
 
   return (
-    <View>
-      <Text>{item?.title}</Text>
-      <Text>{item?.content}</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={100} style={styles.container}>
+      <ItemInputForm title={title} content={content} onChangeTitle={setTitle} onChangeContent={setContent} />
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  }
+});
