51c8826f00769184fbc3b520b5413abd005ae246
shrimp78 <y.kitamura78@gmail.com>
2025-05-04 09:38:07 +0900

    indexで読み込む時にItemServiceを使うように変更

--- app/items/index.tsx
+++ app/items/index.tsx
@@ -4,7 +4,7 @@ import { router, useNavigation } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
 import { Item } from '../../src/components/types/item';
 import { ItemList } from '../../src/components/items/ItemList';
-import { DUMMY_ITEMS } from '../../src/data/dummyItemData';
+import * as ItemService from '../../src/services/itemService';
 
 export default function ItemScreen() {
   const [items, setItems] = useState<Item[]>([]);
@@ -15,6 +15,15 @@ export default function ItemScreen() {
     navigation.setOptions({
       headerRight: () => <Feather name="edit" size={24} onPress={handleAddItemPress} />
     });
+  }, []);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const items = await ItemService.getAllItems();
+      setItems(items);
+    };
+
+    fetchItems();
   }, []);
 
   // 追加ボタン押下時の処理
@@ -33,7 +42,7 @@ export default function ItemScreen() {
     <View style={styles.container}>
       <FlatList
         contentContainerStyle={{ paddingBottom: 120 }}
-        data={DUMMY_ITEMS}
+        data={items}
         renderItem={({ item }) => <ItemList name={item.title} content={item.content} onPress={() => handleItemPress(item.id)} />}
       />
     </View>
