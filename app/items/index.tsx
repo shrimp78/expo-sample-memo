19ba9de40dc00e1f1d5c0865411d2f40506d1459
shrimp78 <y.kitamura78@gmail.com>
2025-05-08 07:44:07 +0900

    Item追加ボタンを画面右下に移動

--- app/items/index.tsx
+++ app/items/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, FlatList, Button } from 'react-native';
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { useState, useEffect, useCallback } from 'react';
 import { router, useNavigation, useFocusEffect } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
@@ -8,14 +8,6 @@ import * as ItemService from '../../src/services/itemService';
 
 export default function ItemScreen() {
   const [items, setItems] = useState<Item[]>([]);
-
-  // ヘッダーの追加ボタン表示
-  const navigation = useNavigation();
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => <Feather name="edit" size={24} onPress={handleAddItemPress} />
-    });
-  }, []);
 
   useFocusEffect(
     useCallback(() => {
@@ -47,6 +39,9 @@ export default function ItemScreen() {
         data={items}
         renderItem={({ item }) => <ItemList name={item.title} content={item.content} onPress={() => handleItemPress(item.id)} />}
       />
+      <TouchableOpacity style={styles.floatingButton} onPress={handleAddItemPress}>
+        <Feather name="plus" size={24} color="gray" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -55,5 +50,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#efeff4'
+  },
+  floatingButton: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 40,
+    elevation: 5, // Android用の影
+    shadowColor: 'black', // iOS用の影
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    borderRadius: 30
   }
 });
