def6c2001a54faf4e70408a4d197be199da3b609
shrimp78 <y.kitamura78@gmail.com>
2025-06-08 07:20:56 +0900

    セレクトのパーツの使用を辞める

--- src/components/items/ItemInputForm.tsx
+++ src/components/items/ItemInputForm.tsx
@@ -1,23 +1,6 @@
-import {
-  Input,
-  InputField,
-  Textarea,
-  TextareaInput,
-  Select,
-  SelectTrigger,
-  SelectInput,
-  SelectIcon,
-  SelectPortal,
-  SelectBackdrop,
-  SelectContent,
-  SelectDragIndicatorWrapper,
-  SelectDragIndicator,
-  SelectItem
-} from '@gluestack-ui/themed';
+import { Input, InputField, Textarea, TextareaInput } from '@gluestack-ui/themed';
 import { InputAccessoryView, View, Platform } from 'react-native';
 import { KeyboardCloseButton } from './KeyboardCloseButton';
-import Entypo from '@expo/vector-icons/Entypo';
-import { useState } from 'react';
 
 type ItemInputFormProps = {
   title: string;
@@ -52,29 +35,8 @@ const ItemInputForm: React.FC<ItemInputFormProps> = props => {
         />
       </Input>
 
-      {/* グループ選択 */}
-      <Select>
-        <SelectTrigger variant="outline" size="md" style={{ zIndex: 1000 }}>
-          <SelectInput placeholder="グループを選択" />
-          <SelectIcon>
-            <Entypo name="chevron-down" size={20} />
-          </SelectIcon>
-        </SelectTrigger>
-        <SelectPortal>
-          <SelectBackdrop />
-          <SelectContent style={{ zIndex: 2000 }}>
-            <SelectDragIndicatorWrapper>
-              <SelectDragIndicator />
-            </SelectDragIndicatorWrapper>
-            <SelectItem label="仕事" value="work" />
-            <SelectItem label="プライベート" value="private" />
-            <SelectItem label="その他" value="other" />
-          </SelectContent>
-        </SelectPortal>
-      </Select>
-
       {/* 内容入力 */}
-      <Textarea borderWidth={0} minWidth={'$full'} minHeight={'$full'} marginTop={'$16'}>
+      <Textarea borderWidth={0} minWidth={'$full'} minHeight={'$full'} marginTop={'$4'}>
         <TextareaInput
           placeholder="内容を入力してください"
           value={content}
