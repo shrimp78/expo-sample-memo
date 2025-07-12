3c7c43e0b6a595edc8b3aa0389b29ef313f66fb3
shrimp78 <y.kitamura78@gmail.com>
2025-07-13 07:19:14 +0900

    カラーオプションは外から

--- src/components/groups/groupColorSelector.tsx
+++ src/components/groups/groupColorSelector.tsx
@@ -1,37 +1,12 @@
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { HStack } from '@gluestack-ui/themed';
 import { FontAwesome } from '@expo/vector-icons';
+import { colorOptions } from '../../../constants/colors';
 
 type GroupColorSelectorProps = {
   groupColor: string;
   onChangeGroupColor: (color: string) => void;
 };
-
-// グループカラーのプリセット（マテリアルカラー500番）
-const colorOptions = [
-  '#2196f3', // Blue 500
-  '#03a9f4', // Light Blue 500
-  '#00bcd4', // Cyan 500
-  '#3f51b5', // Indigo 500
-  '#f44336', // Red 500
-  '#e91e63', // Pink 500
-  '#9c27b0', // Purple 500
-  '#673ab7', // Deep Purple 500
-  '#009688', // Teal 500
-  '#4caf50', // Green 500
-  '#8bc34a', // Light Green 500
-  '#cddc39', // Lime 500
-  '#ffeb3b', // Yellow 500
-  '#ffc107', // Amber 500
-  '#ff9800', // Orange 500
-  '#ff5722', // Deep Orange 500
-  '#795548', // Brown 500
-  '#9e9e9e', // Grey 500
-  '#607d8b', // Blue Grey 500
-  '#37474f', // Blue Grey 800
-  '#424242', // Grey 800
-  '#000000' // Black
-];
 
 const GroupColorSelector: React.FC<GroupColorSelectorProps> = props => {
   const { groupColor, onChangeGroupColor } = props;
