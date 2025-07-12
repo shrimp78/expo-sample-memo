d50c65b683d5dcd328db04eaa1b4787125d4355e
shrimp78 <y.kitamura78@gmail.com>
2025-07-13 07:18:29 +0900

    定数のキャンプ地はここ

---
+++ constants/colors.ts
@@ -0,0 +1,25 @@
+// グループカラーのプリセット（マテリアルカラー500番）
+export const colorOptions = [
+  '#2196f3', // Blue 500
+  '#03a9f4', // Light Blue 500
+  '#00bcd4', // Cyan 500
+  '#3f51b5', // Indigo 500
+  '#f44336', // Red 500
+  '#e91e63', // Pink 500
+  '#9c27b0', // Purple 500
+  '#673ab7', // Deep Purple 500
+  '#009688', // Teal 500
+  '#4caf50', // Green 500
+  '#8bc34a', // Light Green 500
+  '#cddc39', // Lime 500
+  '#ffeb3b', // Yellow 500
+  '#ffc107', // Amber 500
+  '#ff9800', // Orange 500
+  '#ff5722', // Deep Orange 500
+  '#795548', // Brown 500
+  '#9e9e9e', // Grey 500
+  '#607d8b', // Blue Grey 500
+  '#37474f', // Blue Grey 800
+  '#424242', // Grey 800
+  '#000000' // Black
+];
