735ca282f984e095e0f13eee211826c67541408e
shrimp78 <y.kitamura78@gmail.com>
2025-07-21 06:25:09 +0900

    group選択状態をより目立たしたい

--- src/components/screens/groups/groupColorSelector.tsx
+++ src/components/screens/groups/groupColorSelector.tsx
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f0',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 4
   }
