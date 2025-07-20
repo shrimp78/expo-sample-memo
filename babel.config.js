1a069ebd5ce21a4d9980cb8fa236f538a847c755
shrimp78 <y.kitamura78@gmail.com>
2025-07-21 07:06:42 +0900

    module-resolver いれる

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,23 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/components/screens',
+            '@services': './src/services',
+            '@context': './src/context',
+            '@models': './src/components/types',
+            '@constants': './constants'
+          }
+        }
+      ]
+    ]
   };
 };
