9d147e65fcadfc3289be55a5a0a1a574adb641e0
shrimp78 <y.kitamura78@gmail.com>
2025-07-23 05:54:10 +0900

    CLIENT ID をenvから読み込む

---
+++ app.config.js
@@ -0,0 +1,35 @@
+export default {
+  expo: {
+    name: 'mt-app',
+    slug: 'mt-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.shrimp78.mt-app'
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      },
+      package: 'com.shrimp78.mt-app'
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    },
+    plugins: ['expo-router', 'expo-sqlite'],
+    extra: {
+      iosClientId: process.env.IOS_GCP_OAUTH_CLIENT_ID
+    }
+  },
+  scheme: 'mt-app'
+};
