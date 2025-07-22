10e39fe0f128f58d1b3588d4fe80e54e222c8efd
shrimp78 <y.kitamura78@gmail.com>
2025-07-23 06:17:56 +0900

    AuthProviderで全体をラップ

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,31 +3,34 @@ import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { GroupProvider } from '@context/GroupContext';
+import { AuthProvider } from '@context/AuthContext';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <GluestackUIProvider config={config}>
-        <GroupProvider>
-          <Stack
-            screenOptions={{
-              headerTintColor: '#000000',
-              headerStyle: { backgroundColor: '#F9F9F9' }
-            }}
-          >
-            <Stack.Screen name="index" options={{ headerShown: false }} />
+        <AuthProvider>
+          <GroupProvider>
+            <Stack
+              screenOptions={{
+                headerTintColor: '#000000',
+                headerStyle: { backgroundColor: '#F9F9F9' }
+              }}
+            >
+              <Stack.Screen name="index" options={{ headerShown: false }} />
 
-            {/* ホーム */}
-            <Stack.Screen name="home/index" options={{ headerTitle: '' }} />
+              {/* ホーム */}
+              <Stack.Screen name="home/index" options={{ headerTitle: '' }} />
 
-            {/* アイテム */}
-            <Stack.Screen name="items/[id]" options={{ headerTitle: '' }} />
+              {/* アイテム */}
+              <Stack.Screen name="items/[id]" options={{ headerTitle: '' }} />
 
-            {/* グループ */}
-            <Stack.Screen name="groups/index" options={{ headerTitle: '' }} />
-            <Stack.Screen name="groups/[id]" options={{ headerTitle: '' }} />
-          </Stack>
-        </GroupProvider>
+              {/* グループ */}
+              <Stack.Screen name="groups/index" options={{ headerTitle: '' }} />
+              <Stack.Screen name="groups/[id]" options={{ headerTitle: '' }} />
+            </Stack>
+          </GroupProvider>
+        </AuthProvider>
       </GluestackUIProvider>
     </GestureHandlerRootView>
   );
