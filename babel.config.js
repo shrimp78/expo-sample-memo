e4d50de4837b8b596404fcffc6ef89420fbee6d4
shrimp78 <y.kitamura78@gmail.com>
2025-07-26 14:33:14 +0900

    babel-plugin-dotenvの追加

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'babel-plugin-dotenv',
       'react-native-reanimated/plugin',
       [
         'module-resolver',
