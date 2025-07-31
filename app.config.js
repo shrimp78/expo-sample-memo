39024b12bf80c4eb450837d158dc17750f6fe9f1
shrimp78 <y.kitamura78@gmail.com>
2025-08-01 06:49:39 +0900

    app config にもfirestore追加

--- app.config.js
+++ app.config.js
@@ -33,6 +33,7 @@ export default {
       'expo-sqlite',
       '@react-native-firebase/app',
       '@react-native-firebase/auth',
+      '@react-native-firebase/firestore',
       '@react-native-google-signin/google-signin'
     ],
     extra: {
