f2849eca3a66e6a23944228249386e31657b19d2
shrimp78 <y.kitamura78@gmail.com>
2025-08-07 06:02:38 +0900

    dotenvを読むライブラリをチェンジ

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'babel-plugin-dotenv',
+      'module:react-native-dotenv',
       'react-native-reanimated/plugin',
       [
         'module-resolver',
