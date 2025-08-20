dd7ec7ceda1151b9e861103e8043b6bf0d23cc27
shrimp78 <y.kitamura78@gmail.com>
2025-08-21 06:40:35 +0900

    Change title

--- src/components/screens/auth/LoginScreen.tsx
+++ src/components/screens/auth/LoginScreen.tsx
@@ -37,7 +37,7 @@ export default function LoginScreen() {
     <View style={styles.container}>
       {/* アプリロゴ・タイトル */}
       <View style={styles.headerSection}>
-        <Text style={styles.appTitle}>EXPO SAMPLE</Text>
+        <Text style={styles.appTitle}>Special Days</Text>
       </View>
 
       {/* ログインセクション */}
