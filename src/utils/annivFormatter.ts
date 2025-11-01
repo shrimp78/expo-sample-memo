2790176def45248c869a510b3f2c073409b06e78
shrimp78 <y.kitamura78@gmail.com>
2025-11-02 06:16:29 +0900

    changeAnnivFormatとして中身をちゃんと書いた

--- src/utils/annivFormatter.ts
+++ src/utils/annivFormatter.ts
@@ -1,15 +1,40 @@
-export default function annivFormatter(annivDate: Date): number {
-  const date = new Date(annivDate);
+/**
+ * Annivから経過年数と単位を計算して返す関数
+ * @param annivDate Anniv
+ * @returns { number: number; unit: string } 年数と単位
+ */
+export function changeAnnivFormat(anniv: Date): { number: number; unit: string } {
+  let years = 0;
+  let months = 0;
+  let returnNumber = 0;
+
+  // 経過年数を計算
   const now = new Date();
+  years = now.getFullYear() - anniv.getFullYear();
+  const hasHadAnnivThisYear =
+    now.getMonth() > anniv.getMonth() ||
+    (now.getMonth() === anniv.getMonth() && now.getDate() >= anniv.getDate());
 
-  let years = now.getFullYear() - date.getFullYear();
-  const hasHadAnniversaryThisYear =
-    now.getMonth() > date.getMonth() ||
-    (now.getMonth() === date.getMonth() && now.getDate() >= date.getDate());
-
-  if (!hasHadAnniversaryThisYear) {
+  if (!hasHadAnnivThisYear) {
     years -= 1;
   }
 
-  return Math.max(0, years);
+  // 経過年が0の場合は、経過月数を計算
+  if (years === 0) {
+    const totalMonthsDiff = 12 + (now.getMonth() - anniv.getMonth());
+    months = totalMonthsDiff - (now.getDate() < anniv.getDate() ? 1 : 0);
+  }
+  returnNumber = years === 0 ? months : years;
+
+  // 単位を計算
+  let unit = '';
+  if (years === 0) {
+    unit = 'months';
+  } else if (years === 1) {
+    unit = 'year';
+  } else {
+    unit = 'years';
+  }
+
+  return { number: returnNumber, unit: unit };
 }
