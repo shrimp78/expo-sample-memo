e8209dc46abdbef9cc3bc208db26cc9327f93cb8
shrimp78 <y.kitamura78@gmail.com>
2026-01-04 06:45:34 +0900

    ItemType に通知用データを追加

--- src/components/types/Item.ts
+++ src/components/types/Item.ts
@@ -8,6 +8,10 @@ type Item = {
   content: string | null;
   group_id: string | null;
   birthday: Timestamp;
+  notifyEnabled: boolean | null;
+  notifyTiming: string | null; // '1h', '24h', '7d', '14d', '30d
+  nextNotifyAt: Timestamp | null; // 次回通知予定日時
+  lastNotifiedAt: Timestamp | null; // 最後に通知を送った日時
 };
 
 export type { Item };
